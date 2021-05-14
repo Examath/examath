6c2d8bfe09ccf9864cf4d12c120fc497156247d1
alphax10 <pauljis@outlook.com>
2021-05-14 22:03:55 +0930

    Dependable class update

--- ASO.js
+++ ASO.js
@@ -1,8 +1,68 @@
 /**
+ * Akin to C# DependencyProperty
+ */
+class Dependable {
+    /**@private */
+    Listeners = [];
+    /**@private */
+    value;
+    /**
+     * Creates a dependable variable
+     * @param {*} value Sets the default value
+     * @param {function} callback Adds a listener to the dependable's callback list
+     */
+    constructor(value, callback = undefined) {
+        this.value = value;
+        if (callback) {
+            this.AddListener(callback);
+        }
+    }
+    /**
+     * Gets the value of this dependable, or 
+     */
+    get Value() {
+        return this.value;
+    }    
+    /**
+     * Sets the value of this dependable and calls all it's listeners
+     */
+    set Value(x) {
+        this.value = x;
+        this.Listeners.forEach((e) => {
+            e();
+        })
+    }
+    /**
+     * Subscribes a listening method onto this dependable
+     * @param {function} callback The function to run when called back
+     */
+    AddListener(callback) {
+        this.Listeners.push(callback);
+    }
+}
+
+/**
+ * Marks a <input> element as a toggle button controling the specified dependable
+ * @param {Element} e The input button element target
+ * @param {Dependable} variable A object of the Dependable class of which the toggle button will toggle
+ * @example <caption>To define a toggle button, call this function in the onload of a input[type=button]</caption>
+ * <input type="button" onload="MarkAsToggle(this, Dep)"/>
+ */
+function MarkAsToggle(e, variable) {
+    e.classList.add("toggle");
+    e.addEventListener("click", function () {
+        variable.Value = (variable.Value) ? false : true;
+    })
+    variable.AddListener(function () {        
+        SwitchClass(e, "check", variable.Value);
+    })
+}
+/**
  * Flips a toggle input and a variable.
  * @param {Element} e input.toggle target element
  * @param {Boolean} val Initial value
  * @returns {Boolean} Changed value
+ * @deprecated since Dependenable Class update
  */
 function Check(e, val) {
     SwitchClass(e, "check", val);
@@ -24,7 +84,7 @@ function SwitchClass(e, clas, val) {
 }
 
 /**
- * Similar to {@link document.getElementById}
+ * Similar to{@link document.getElementById}
  * @param {String} id Id of target element
  */
 function Get(id) {
@@ -86,10 +146,18 @@ function HslToHex(h, s, l) {
     return `#${f(0)}${f(8)}${f(4)}`;
 }
 // Footer
-var vstr = "";
-var vel = document.querySelector(`meta[name="version"]`);
-if (vel) {
-    vstr = " | Version " + vel.getAttribute('content');
-}
-
-document.body.innerHTML += `<div class="notice" title="This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.">Copyright &copy; <a href="https://github.com/alphax10">Paul Cyril</a> ${GetMeta("year", "2021")}</a>, <a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html">GNU GPL v2.0</a>${vstr}</div>`;
+document.addEventListener("DOMContentLoaded", () => {
+    // Version Info
+    var vstr = "";
+    var vel = document.querySelector(`meta[name="version"]`);
+    if (vel) {
+        vstr = " | Version " + vel.getAttribute('content');
+    }
+    document.body.innerHTML += `<div class="notice" title="This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.">Copyright &copy; <a href="https://github.com/alphax10">Paul Cyril</a> ${GetMeta("year", "2021")}</a>, <a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html">GNU GPL v2.0</a>${vstr}</div>`;    
+    // Onload on input
+    document.querySelectorAll("input[onload]").forEach(function (e) {
+        e.dispatchEvent(new Event("load"));
+    })
+    //
+    console.log("ASO.js loaded")
+})
