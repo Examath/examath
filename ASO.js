f296084cb7324662258c27ba2652f90189604233
alphax10 <pauljis@outlook.com>
2022-09-30 12:50:07 +0930

    Partial HTML

--- ASO.js
+++ ASO.js
@@ -467,7 +467,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.innerHTML += `<div class="notice" title="This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.">Copyright &copy; <a href="https://github.com/alphax10">Paul Cyril</a> ${GetMeta("year", "2021")}</a>, <a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html">GNU GPL v2.0</a>${vstr}</div>`;
 
     // Onload on input
-    document.querySelectorAll("input[onload]").forEach(function (e) {
+    document.querySelectorAll("button[onload]").forEach(function (e) {
         e.dispatchEvent(new Event("load"));
     })
 
